cf2d5168422c45faaa8a9dcda8c8b35f8b26ccd9
HiMoh57 <cureplushelp@gmail.com>
2026-07-06 19:40:19 +0530

    fix: package preview dependencies and path resolutions

--- packages/preview/vite.config.ts
+++ packages/preview/vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '/pdfs': path.resolve(process.cwd(), 'pdfs'),
-      '@teamless/react': path.resolve(__dirname, '../react/src'),
     },
   },
 });
